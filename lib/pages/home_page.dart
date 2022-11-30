a6c7abdf46715678059aa5d8d4c2bd1af37c6c9f
Edgar <mmnledgar@gmail.com>
2022-11-30 21:03:20 +0000

    setting up page: reproduce design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart' ;
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
@@ -40,20 +43,56 @@ class HomePage extends StatelessWidget{
       ),
       body: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              "Find The Best Coffee For You",
-              style: GoogleFonts.lato(
-                  textStyle: TextStyle(
-                      fontSize: 36,
-                    fontWeight: FontWeight.bold
-                  )
-              ),
-            ),
-          )
+          // The Text: Find the best coffee for you
+          this.getTitle(),
+
+          SizedBox(
+            height: 25,
+          ),
+
+          // search bar
+          this.getSearchField()
+
+          // list of propositions
         ],
       )
+    );
+  }
+
+
+  Widget getTitle(){
+    return Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Text(
+          "Find The Best Coffee For You",
+          style: GoogleFonts.bebasNeue(
+              textStyle: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+        ),
+      );
+  }
+
+  Widget getSearchField(){
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.search,
+          ),
+          hintText: 'Find your coffee...',
+          focusedBorder: OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey.shade600,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(20.0))
+          )
+        ),
+      ),
     );
   }
 }
