d7691ae8e2d2e569d8ca7f61d2e7d0b22bbc026f
Edgar <mmnledgar@gmail.com>
2022-11-30 20:16:14 +0000

    setting up page: reproduce design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tdd/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,11 +16,3 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class HomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
