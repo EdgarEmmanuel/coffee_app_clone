79c89ae7373971928a02e41c35cc5ff95e5d7b1f
Edgar <mmnledgar@gmail.com>
2022-11-30 20:40:43 +0000

    setting up page: reproduce design

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage()
+        home: HomePage(),
+      theme: ThemeData(
+        brightness: Brightness.dark
+      )
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart' ;
+import 'package:google_fonts/google_fonts.dart';
 
 
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.menu
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+                Icons.person
+            )
+          )
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('')
+            ),
+          BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                title: Text('')
+            ),
+          BottomNavigationBarItem(
+                icon: Icon(Icons.notifications),
+                title: Text('')
+            ),
         ],
       ),
       body: Column(
         children: [
-
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "Find The Best Coffee For You",
+              style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 36,
+                    fontWeight: FontWeight.bold
+                  )
+              ),
+            ),
+          )
         ],
       )
     );
