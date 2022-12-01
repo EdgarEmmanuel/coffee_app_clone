36c8fdabaea93344e32dc01e17cf8cdcaea0883b
Edgar <mmnledgar@gmail.com>
2022-12-01 17:53:02 +0000

    adding others widgets

--- lib/util/coffee_tile.dart
+++ lib/util/coffee_tile.dart
@@ -8,10 +8,73 @@ class CoffeeTile extends StatelessWidget{
     return Padding(
       padding: EdgeInsets.only(left: 10.0, bottom: 25.0),
       child: Container(
-        width: 200,
+        padding: EdgeInsets.all(12.0),
+        width: 150,
         decoration: BoxDecoration(
-          color: Colors.grey,
+          color: Colors.black54,
           borderRadius: BorderRadius.circular(10.0)
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            // image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.network(
+                "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+                height: 200,
+                alignment: Alignment.center,
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // title of the coffee
+                  Text(
+                    "Cappucino",
+                    style: GoogleFonts.bebasNeue(
+                        textStyle: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        )
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+
+
+                  // description of the coffee
+                  Text(
+                    "With dark milk",
+                    style: GoogleFonts.adamina(
+                        textStyle: TextStyle(
+                            color: Colors.grey[700],
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w100
+                        ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Row(
+                children: [
+
+                ],
+              ),
+            )
+
+
+
+          ],
         ),
       )
     );
