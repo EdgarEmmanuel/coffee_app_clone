86447de67230dad3884a78e2c8fb3618c443c5ce
Edgar <mmnledgar@gmail.com>
2022-12-13 11:14:23 +0000

    woriking on the tiles

--- lib/util/coffee_type_name.dart
+++ lib/util/coffee_type_name.dart
@@ -5,8 +5,12 @@ import 'package:google_fonts/google_fonts.dart';
 class CoffeeType extends StatelessWidget{
   final String name;
   final bool isSelected;
+  final VoidCallback onTap;
 
-  const CoffeeType({ @required this.name, this.isSelected = false });
+  const CoffeeType({
+    @required this.name,
+    @required this.onTap,
+    this.isSelected = false });
 
 
 
@@ -17,15 +21,18 @@ class CoffeeType extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 10.0, left: 20.0),
-      child: Text(
-        "${this.name}",
-        style: GoogleFonts.nunito(
-          textStyle: TextStyle(
-              color: getTheRightColor(),
-              fontSize: 12.0,
-              fontWeight: FontWeight.bold
+    return GestureDetector(
+      onTap: this.onTap,
+      child: Container(
+        margin: EdgeInsets.only(right: 10.0, left: 20.0),
+        child: Text(
+          "${this.name}",
+          style: GoogleFonts.nunito(
+            textStyle: TextStyle(
+                color: getTheRightColor(),
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold
+            ),
           ),
         ),
       ),
